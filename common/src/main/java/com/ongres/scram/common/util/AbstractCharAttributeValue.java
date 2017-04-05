e82dec14e7a6104dc3c2fd3574df46e45952717d
Álvaro Hernández Tortosa <aht@ongres.com>
2017-04-05 03:26:52 +0200

    Small improvements to AbstractCharAttributeValue
    Make fields private!

--- common/src/main/java/com/ongres/scram/common/util/AbstractCharAttributeValue.java
+++ common/src/main/java/com/ongres/scram/common/util/AbstractCharAttributeValue.java
@@ -28,15 +28,15 @@ import static com.ongres.scram.common.util.Preconditions.checkNotNull;
 
 
 /**
- * Construct and write generic CharAttibute-Value pairs.
+ * Construct and write generic CharAttribute-Value pairs.
  *
  * Concrete sub-classes should also provide a static parse(String) creation method.
  */
 public class AbstractCharAttributeValue extends AbstractStringWritable implements CharAttributeValue {
-    public final CharAttribute charAttribute;
-    public final String value;
+    private final CharAttribute charAttribute;
+    private final String value;
 
-    public AbstractCharAttributeValue(CharAttribute charAttribute, String value) {
+    public AbstractCharAttributeValue(CharAttribute charAttribute, String value) throws IllegalArgumentException {
         this.charAttribute = checkNotNull(charAttribute, "attribute");
         if(null != value && value.isEmpty()) {
             throw new IllegalArgumentException("Value should be either null or non-empty");
