d5b735d1598d63954ae3a62b2b77bf0d88a30bab
Matteo Melli <matteom@ongres.com>
2019-06-27 13:14:01 +0200

    Change copyright year to 2019

--- common/src/main/java/com/ongres/scram/common/pbkdf2/PBEParametersGenerator.java
+++ common/src/main/java/com/ongres/scram/common/pbkdf2/PBEParametersGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, OnGres.
+ * Copyright 2019, OnGres.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
