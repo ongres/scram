b8379941c72cafb42a7d4b7c3797469dd2a82081
Jorge Solórzano <jorsol@gmail.com>
2024-08-19 22:11:36 +0200

    test: Add a high iteration count of 1000000
    Signed-off-by: Jorge Solórzano <jorsol@gmail.com>

--- scram-client/src/test/java/com/example/ScramClientTest.java
+++ scram-client/src/test/java/com/example/ScramClientTest.java
@@ -15,11 +15,9 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
-import java.util.Base64;
 
 import com.ongres.scram.client.ScramClient;
 import com.ongres.scram.common.ClientFinalMessage;
-import com.ongres.scram.common.exception.ScramParseException;
 import com.ongres.scram.common.util.TlsServerEndpoint;
 import org.junit.jupiter.api.Test;
 
@@ -75,5 +73,39 @@ class ScramClientTest {
       assertEquals("SHA512withRSA", cert.getSigAlgName());
     }
     return cert;
+  }
+
+  @Test
+  void iterationTest()
+      throws CertificateException, IOException {
+    final X509Certificate cert = getCert();
+    final byte[] channelBindingData = TlsServerEndpoint.getChannelBindingData(cert);
+
+    ScramClient scramSession = ScramClient.builder()
+        .advertisedMechanisms(Arrays.asList("SCRAM-SHA-256"))
+        .username("postgres")
+        .password("pencil".toCharArray())
+        .channelBinding(TlsServerEndpoint.TLS_SERVER_END_POINT, channelBindingData)
+        .nonceSupplier(() -> "1q^MGrWUi{etW+H7(#k431kB")
+        .build();
+    assertEquals("SCRAM-SHA-256", scramSession.getScramMechanism().getName());
+    assertEquals("y,,n=postgres,r=1q^MGrWUi{etW+H7(#k431kB",
+        scramSession.clientFirstMessage().toString());
+
+    assertDoesNotThrow(
+        () -> scramSession.serverFirstMessage(
+            "r=1q^MGrWUi{etW+H7(#k431kBdAr3CWX7B6houDP4f7Z2XEpZ,"
+                + "s=Fgh8JU2AlRjBHUsIU/GgtQ==,"
+                + "i=1000000"));
+
+    ClientFinalMessage clientFinalMessage = scramSession.clientFinalMessage();
+    assertEquals(
+        "c=eSws,"
+            + "r=1q^MGrWUi{etW+H7(#k431kBdAr3CWX7B6houDP4f7Z2XEpZ,"
+            + "p=vQ3IyYl3LvjWOlK2c0IP5QAi6XB7Dm0Axo0V51DcHZA=",
+        clientFinalMessage.toString());
+
+    assertDoesNotThrow(
+        () -> scramSession.serverFinalMessage("v=sz/isCwVSUn/TBWeYABz6WaoZIcfsui9NPaJCoxxAjY="));
   }
 }
